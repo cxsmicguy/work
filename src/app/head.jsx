94de2da296bb4bd1355f16fdf0114d0f916c7f7c
cxsmicguy <cxsmicguy@gmail.com>
2023-04-01 17:46:12 +0530

    added fav

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>My Works - Cxsmivguy</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.svg" />
+      <link rel="icon" href="/favicon.png" />
     </>
   )
 }
