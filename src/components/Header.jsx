2358197dd87d5130651d9ef11eaa153bdbc7cf8a
cxsmicguy <cxsmicguy@gmail.com>
2023-04-02 11:31:59 +0530

    a minor ui update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ export default function Header() {
   return (
     <div className="relative text-white pt-12 h-screen">
       <center>
-        <img src="/me.jpg" alt="me" className="rounded-full border-2 border-gray-400 w-1/5 min-w-12 mt-12 max-w-xs" />
+        <img src="/me.jpg" alt="me" className="rounded-full border-2 border-gray-400 w-1/5 min-w-24 mt-12 max-w-xs" />
         <br />
         <h2 className="text-3xl font-bold">Cxsmic Guy</h2>
-        <span className="text-white px-4 py-1 bg-gradient-to-r from-sky-500 to-indigo-500 inline-block mt-4 rounded-full">My works</span>
+        <span className="text-white px-4 py-1 bg-gradient-to-r from-sky-500 to-indigo-500 inline-block mt-4 rounded-full">FEATURED WORKS</span>
         <div className="bottom-8 p-8 block w-full absolute">
           <center>
             <FiChevronsDown size={30}  />
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,6 @@
 export default function Portfolio() {
   return (
-    <div className="bottom-0 left-0 py-5 fixed text-center w-full">
+    <div className="top-0 left-0 py-5 fixed text-center w-full">
       <a href="http://cxsmicguy.com" className="bg-gray-900 opacity-70 px-4 py-2 rounded-full text-white font-semibold">
         Portfolio
       </a>
